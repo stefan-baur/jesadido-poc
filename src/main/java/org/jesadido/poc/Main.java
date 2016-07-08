470e7060235f0420d8991eff25ba75c4b2ce9250
Stefan K. Baur <Stefan K. Baur@HomeLab>
2016-07-08 16:38:45 +0200

    Licensing for clarifying that this project stays under LGPL 3.0

---
+++ src/main/java/org/jesadido/poc/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2016 Stefan K. Baur
+ *
+ * Licensed under the GNU Lesser Public License, Version 3.0 (LGPL 3.0)
+ * https://www.gnu.org/licenses/lgpl-3.0.txt
+ */
+package org.jesadido.poc;
+
+public class Main {
+    
+    public static void main(String[] arguments) {
+        
+        System.out.println("Jesadido (POC)");
+    }
+}
