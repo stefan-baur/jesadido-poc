7299a7cf274eb4e0cfcb9108686134c7ff0a853b
Stefan K. Baur <info@stefan-baur.org>
2016-07-17 17:11:29 +0200

    Renaming from ConceptPool to ConceptRegistry

--- src/main/java/org/jesadido/poc/core/concepts/ConceptRegistry.java
+++ src/main/java/org/jesadido/poc/core/concepts/ConceptRegistry.java
@@ -9,16 +9,15 @@ package org.jesadido.poc.core.concepts;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Logger;
 
-public final class ConceptPool {
+public final class ConceptRegistry {
     
-    private static final Map<String, Concept> CONCEPTS;
+    private static final Logger LOGGER = Logger.getLogger(ConceptRegistry.class.getName());
     
-    static {
-        CONCEPTS = new HashMap<>();
-    }
+    private static final Map<String, Concept> CONCEPTS = new HashMap<>();
     
-    private ConceptPool() {
+    private ConceptRegistry() {
         // Unused.
     }
     
@@ -32,7 +31,11 @@ public final class ConceptPool {
     
     public static final Concept getConcept(final String conceptPhrase) {
         if (!CONCEPTS.containsKey(conceptPhrase)) {
-            CONCEPTS.put(conceptPhrase, ConceptParser.parse(conceptPhrase));
+            Concept parsedConcept = ConceptParser.parse(conceptPhrase);
+            if (!conceptPhrase.equals(parsedConcept.getFullPhrase())) {
+                LOGGER.warning(String.format("The given concept phrase differs to the parsed result: \"%s\" != \"%s\"", conceptPhrase, parsedConcept.getFullPhrase()));
+            }
+            CONCEPTS.put(conceptPhrase, parsedConcept);
         }
         return CONCEPTS.get(conceptPhrase);
     }
