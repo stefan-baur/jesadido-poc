02d785f9613ad0b036a2a706c0390ce1397152cf
Stefan K. Baur <info@stefan-baur.org>
2016-07-19 06:44:16 +0200

    Concept phrase parsing is finished for now

--- src/main/java/org/jesadido/poc/core/concepts/ConceptParser.java
+++ src/main/java/org/jesadido/poc/core/concepts/ConceptParser.java
@@ -7,8 +7,9 @@
  */
 package org.jesadido.poc.core.concepts;
 
-import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import org.jesadido.poc.core.CoreUtils;
 
 public final class ConceptParser {
     
@@ -16,11 +17,79 @@ public final class ConceptParser {
         // A private utility class constructor.
     }
     
-    public static final Concept parse(final String fullPhrase) {
-        return new Concept(new ConceptBuilder(parseToMorphemes(fullPhrase)));
+    public static final Concept parse(final String conceptPhrase) {
+        return new Concept(new ConceptBuilder(parseToMorphemes(conceptPhrase)));
     }
     
-    private static List<String> parseToMorphemes(final String fullPhrase) {
-        return Arrays.asList(fullPhrase);
+    public static final List<String> parseToMorphemes(final String conceptPhrase) {
+        final List<String> result = new LinkedList<>();
+        final String escaper = CoreUtils.escaper("1", conceptPhrase);
+        final String escapedfullPhrase = conceptPhrase.replace("\\$", escaper);
+        final String[] escapedBasePhrases = escapedfullPhrase.split("$");
+        for (int i = 0; i < escapedBasePhrases.length; i++) {
+            if (i > 0) {
+                result.add("$");
+            }
+            result.addAll(parseBasePhrase(escapedBasePhrases[i].replace(escaper, "\\$")));
+        }
+        return result;
+    }
+    
+    private static List<String> parseBasePhrase(final String basePhrase) {
+        final List<String> result = new LinkedList<>();
+        if (basePhrase.contains("'")) {
+            final int beginIndex = basePhrase.indexOf('\'');
+            final int endIndex = basePhrase.lastIndexOf('\'');
+            final String prefix = basePhrase.substring(0, beginIndex);
+            if (prefix.startsWith("/") && prefix.endsWith("/")) {
+                result.add(prefix);
+            }
+            result.add(basePhrase.substring(beginIndex, endIndex + 1));
+            final String suffix = basePhrase.substring(endIndex + 1, basePhrase.length());
+            if (suffix.length() > 0) {
+                result.addAll(parseBaseFragment(suffix));
+            }
+        } else {
+            result.addAll(parseBaseFragment(basePhrase));
+        }
+        return result;
+    }
+    
+    private static List<String> parseBaseFragment(final String baseFragment) {
+        final List<String> result = new LinkedList<>();
+        boolean number = false;
+        StringBuilder morphemeBuilder = new StringBuilder();
+        for (int i = 0; i < baseFragment.length(); i++) {
+            final char c = baseFragment.charAt(i);
+            if (Character.isUpperCase(c)) {
+                if (morphemeBuilder.length() > 0) {
+                    result.add(morphemeBuilder.toString());
+                    morphemeBuilder = new StringBuilder();
+                }
+                morphemeBuilder.append(c);
+                number = false;
+            } else if (Character.isLowerCase(c)) {
+                morphemeBuilder.append(c);
+                number = false;
+            } else if (Character.isDigit(c)) {
+                if (!number && morphemeBuilder.length() > 0) {
+                    result.add(morphemeBuilder.toString());
+                    morphemeBuilder = new StringBuilder();
+                }
+                morphemeBuilder.append(c);
+                number = true;
+            } else {
+                if (morphemeBuilder.length() > 0) {
+                    result.add(morphemeBuilder.toString());
+                    morphemeBuilder = new StringBuilder();
+                }
+                morphemeBuilder.append(c);
+                number = false;
+            }
+        }
+        if (morphemeBuilder.length() > 0) {
+            result.add(morphemeBuilder.toString());
+        }
+        return result;
     }
 }
