921fd8275cbb6a31928efc008365886b6ff3c3f1
Stefan K. Baur <info@stefan-baur.org>
2016-07-19 08:46:35 +0200

    Unit testing of 'CoreUtils'

--- src/main/java/org/jesadido/poc/core/CoreUtils.java
+++ src/main/java/org/jesadido/poc/core/CoreUtils.java
@@ -40,16 +40,6 @@ public final class CoreUtils {
     }
     
     /**
-     * Returns the given string without the last character.
-     * @param value The given string.
-     * @return The given string without the last character. An empty string, if
-     * the given string is empty or null.
-     */
-    public static final String cut(final String value) {
-        return (value == null || value.length() == 0) ? "" : value.substring(0, value.length() - 1);
-    }
-    
-    /**
      * Gernerates a string which is not a part of the given phrase. This is
      * needful for string replacements and its back-replacements.
      * @param escaperPrefix The prefix of the generated result.
---
+++ src/test/java/org/jesadido/poc/core/CoreUtilsTest.java
@@ -0,0 +1,40 @@
+/*
+ * jesadido-poc
+ * Copyright (C) 2016 Stefan K. Baur
+ *
+ * Licensed under the GNU Lesser General Public License, Version 3.0 (LGPL-3.0)
+ * https://www.gnu.org/licenses/lgpl-3.0.txt
+ */
+package org.jesadido.poc.core;
+
+import java.util.Arrays;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CoreUtilsTest {
+    
+    @Test
+    public void testUp() {
+        Assert.assertEquals("Icx", CoreUtils.up("icx"));
+        Assert.assertEquals("Icx", CoreUtils.up("Icx"));
+        Assert.assertEquals("O", CoreUtils.up("o"));
+        Assert.assertEquals("O", CoreUtils.up("O"));
+        Assert.assertArrayEquals(new String[] { "Icx", "O" }, CoreUtils.up(Arrays.asList("icx", "o")).toArray());
+        Assert.assertArrayEquals(new String[] { "Icx", "O" }, CoreUtils.up(Arrays.asList("Icx", "O")).toArray());
+        Assert.assertArrayEquals(new String[] { "'1974-01-28'", "Dat", "O" }, CoreUtils.up(Arrays.asList("'1974-01-28'", "dat", "o")).toArray());
+        Assert.assertArrayEquals(new String[] { "/de/", "'von'Hefner'", "O" }, CoreUtils.up(Arrays.asList("/de/", "'von'Hefner'", "o")).toArray());
+    }
+    
+    @Test
+    public void testEscaper() {
+        Assert.assertEquals("ASDF", CoreUtils.escaper("ASDF", "Ich liebe meine Muttersprache."));
+        Assert.assertEquals("I_", CoreUtils.escaper("I", "Ich liebe meine Muttersprache."));
+        Assert.assertEquals("Mutter_", CoreUtils.escaper("Mutter", "Ich liebe meine Muttersprache."));
+    }
+    
+    @Test
+    public void testToConceptPhrase() {
+        Assert.assertEquals("IcxO", CoreUtils.toConceptPhrase(Arrays.asList("icx", "o")));
+        Assert.assertEquals("/de/'von'Hefner'O", CoreUtils.toConceptPhrase(Arrays.asList("/de/", "'von'Hefner'", "o")));
+    }
+}
