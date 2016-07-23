9dd04f580b3c685277c7be280d1bda19ac096953
Stefan K. Baur <info@stefan-baur.org>
2016-07-23 17:30:11 +0200

    Desicion for CFG (context-free grammar)

---
+++ src/main/java/org/jesadido/poc/core/syntax/cfg/Grammar.java
@@ -0,0 +1,20 @@
+/*
+ * jesadido-poc
+ * Copyright (C) 2016 Stefan K. Baur
+ *
+ * Licensed under the GNU Lesser General Public License, Version 3.0 (LGPL-3.0)
+ * https://www.gnu.org/licenses/lgpl-3.0.txt
+ */
+package org.jesadido.poc.core.syntax.cfg;
+
+import java.util.List;
+import java.util.Map;
+
+public interface Grammar<N, T, P> {
+    
+    String getName();
+    List<N> getNonterminalSymbols();
+    List<T> getTerminalSymbols();
+    Map<String, P> getProductionRules();
+    N getStartSymbol();
+}
