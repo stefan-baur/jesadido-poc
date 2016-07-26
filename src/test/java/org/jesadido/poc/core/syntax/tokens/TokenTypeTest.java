d245a7200933caf720f31ebde8d929af5864cd9c
Stefan K. Baur <info@stefan-baur.org>
2016-07-26 21:32:09 +0200

    Overloading of 'isOneOf'; for List<TokenType> also

--- src/test/java/org/jesadido/poc/core/syntax/tokens/TokenTypeTest.java
+++ src/test/java/org/jesadido/poc/core/syntax/tokens/TokenTypeTest.java
@@ -7,6 +7,7 @@
  */
 package org.jesadido.poc.core.syntax.tokens;
 
+import java.util.Arrays;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -14,14 +15,26 @@ public class TokenTypeTest {
     
     @Test
     public void testIsOneOf() {
-        Assert.assertFalse(TokenType.TERMINATOR.isOneOf());
-        Assert.assertFalse(TokenType.UNKNOWN.isOneOf(TokenType.ADVERB_SINGULAR));
-        Assert.assertFalse(TokenType.UNKNOWN.isOneOf(TokenType.INVALID_CONCEPT, TokenType.ARTICLE));
-        Assert.assertTrue(TokenType.INVALID_CONCEPT.isOneOf(TokenType.INVALID_CONCEPT));
-        Assert.assertTrue(TokenType.INVALID_CONCEPT.isOneOf(TokenType.INVALID_CONCEPT, TokenType.UNKNOWN));
-        Assert.assertTrue(TokenType.INVALID_CONCEPT.isOneOf(TokenType.UNKNOWN, TokenType.INVALID_CONCEPT));
-        Assert.assertTrue(TokenType.INVALID_CONCEPT.isOneOf(TokenType.UNKNOWN, TokenType.INVALID_CONCEPT, TokenType.TERMINATOR));
-        Assert.assertTrue(TokenType.PARAMETERED_SUBSTANTIVE_PLURAL.isOneOf(TokenType.PARAMETERED_SUBSTANTIVE_SINGULAR, TokenType.PARAMETERED_SUBSTANTIVE_PLURAL));
-        Assert.assertTrue(TokenType.ARTICLE.isOneOf(TokenType.values()));
+        {
+            Assert.assertFalse(TokenType.TERMINATOR.isOneOf());
+            Assert.assertFalse(TokenType.UNKNOWN.isOneOf(TokenType.ADVERB_SINGULAR));
+            Assert.assertFalse(TokenType.UNKNOWN.isOneOf(TokenType.INVALID_CONCEPT, TokenType.ARTICLE));
+            Assert.assertTrue(TokenType.INVALID_CONCEPT.isOneOf(TokenType.INVALID_CONCEPT));
+            Assert.assertTrue(TokenType.INVALID_CONCEPT.isOneOf(TokenType.INVALID_CONCEPT, TokenType.UNKNOWN));
+            Assert.assertTrue(TokenType.INVALID_CONCEPT.isOneOf(TokenType.UNKNOWN, TokenType.INVALID_CONCEPT));
+            Assert.assertTrue(TokenType.INVALID_CONCEPT.isOneOf(TokenType.UNKNOWN, TokenType.INVALID_CONCEPT, TokenType.TERMINATOR));
+            Assert.assertTrue(TokenType.PARAMETERED_SUBSTANTIVE_PLURAL.isOneOf(TokenType.PARAMETERED_SUBSTANTIVE_SINGULAR, TokenType.PARAMETERED_SUBSTANTIVE_PLURAL));
+            Assert.assertTrue(TokenType.ARTICLE.isOneOf(TokenType.values()));
+        }
+        {
+            Assert.assertFalse(TokenType.UNKNOWN.isOneOf(Arrays.asList(TokenType.ADVERB_SINGULAR)));
+            Assert.assertFalse(TokenType.UNKNOWN.isOneOf(Arrays.asList(TokenType.INVALID_CONCEPT, TokenType.ARTICLE)));
+            Assert.assertTrue(TokenType.INVALID_CONCEPT.isOneOf(Arrays.asList(TokenType.INVALID_CONCEPT)));
+            Assert.assertTrue(TokenType.INVALID_CONCEPT.isOneOf(Arrays.asList(TokenType.INVALID_CONCEPT, TokenType.UNKNOWN)));
+            Assert.assertTrue(TokenType.INVALID_CONCEPT.isOneOf(Arrays.asList(TokenType.UNKNOWN, TokenType.INVALID_CONCEPT)));
+            Assert.assertTrue(TokenType.INVALID_CONCEPT.isOneOf(Arrays.asList(TokenType.UNKNOWN, TokenType.INVALID_CONCEPT, TokenType.TERMINATOR)));
+            Assert.assertTrue(TokenType.PARAMETERED_SUBSTANTIVE_PLURAL.isOneOf(Arrays.asList(TokenType.PARAMETERED_SUBSTANTIVE_SINGULAR, TokenType.PARAMETERED_SUBSTANTIVE_PLURAL)));
+            Assert.assertTrue(TokenType.ARTICLE.isOneOf(Arrays.asList(TokenType.values())));
+        }
     }
 }
