04596cecc60221318da4a2e5735affaf627706c8
Stefan K. Baur <info@stefan-baur.org>
2016-07-29 21:17:19 +0200

    Rearranging the productions

--- src/main/java/org/jesadido/poc/core/syntax/GrammarFactory.java
+++ src/main/java/org/jesadido/poc/core/syntax/GrammarFactory.java
@@ -22,8 +22,8 @@ public class GrammarFactory {
     
     public final Grammar createBaseGrammar(final String name) {
         return new Grammar(name, new BaseTokenCreator(), new BaseSyntaxTreeFactory())
-                .register(false, new SentenceMeatInfixConjunctionProduction())
                 .register(false, new SentenceMeatProduction())
+                .register(false, new SentenceMeatInfixConjunctionProduction())
                 .register(true, new SentenceInfixConjunctionProduction())
                 .register(false, new SentenceMeatPrefixConjunctionProduction())
                 .register(true, new SentencePrefixConjunctionProduction())
