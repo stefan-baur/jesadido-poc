c0a0ef9a0e2424e174306fdf2eb0b2e377fd5f0c
Stefan K. Baur <info@stefan-baur.org>
2016-08-13 19:35:02 +0200

    Bit of pretty-print-testing

--- src/test/java/org/jesadido/poc/core/syntax/tree/visitors/PrettyPrinterTest.java
+++ src/test/java/org/jesadido/poc/core/syntax/tree/visitors/PrettyPrinterTest.java
@@ -16,51 +16,70 @@ import org.junit.Test;
 public class PrettyPrinterTest {
     
     @Test
-    public void testPrintWithSentence() {
+    public void testAcceptPrettyPrintWithSentence() {
         final Grammar grammar = new GrammarFactory().createJesadidoGrammar();
         {
-            final String pp = PrettyPrinter.print(grammar.parse("HeroO .", Nonterminal.SENTENCE)).toString();
-            final String pppp = PrettyPrinter.print(grammar.parse(pp, Nonterminal.SENTENCE)).toString();
+            final String pp = grammar.parse("HeroO .", Nonterminal.SENTENCE).accept(grammar.getPrettyPrinter(), null).toString();
+            Assert.assertNotNull(pp);
+            Assert.assertTrue(pp.length() > 0);
+            Assert.assertEquals("{\r\n\tSu (\r\n\t\tHeroO\r\n\t)\r\n}\r\n.\r\n", pp);
+            final String pppp = grammar.parse(pp, Nonterminal.SENTENCE).accept(grammar.getPrettyPrinter(), null).toString();
             Assert.assertEquals(pp, pppp);
         }
         {
-            final String pp = PrettyPrinter.print(grammar.parse("TrovAs HeroIcxO Fin SkribIlO .", Nonterminal.SENTENCE)).toString();
-            final String pppp = PrettyPrinter.print(grammar.parse(pp, Nonterminal.SENTENCE)).toString();
+            final String pp = grammar.parse("TrovAs HeroIcxO Fin SkribIlO .", Nonterminal.SENTENCE).accept(grammar.getPrettyPrinter(), null).toString();
+            Assert.assertNotNull(pp);
+            Assert.assertTrue(pp.length() > 0);
+            final String pppp = grammar.parse(pp, Nonterminal.SENTENCE).accept(grammar.getPrettyPrinter(), null).toString();
             Assert.assertEquals(pp, pppp);
         }
         {
-            final String pp = PrettyPrinter.print(grammar.parse("Fin SkribIlO DonAs HeroIcxO Al HeroInO .", Nonterminal.SENTENCE)).toString();
-            final String pppp = PrettyPrinter.print(grammar.parse(pp, Nonterminal.SENTENCE)).toString();
+            final String pp = grammar.parse("Fin SkribIlO DonAs HeroIcxO Al HeroInO .", Nonterminal.SENTENCE).accept(grammar.getPrettyPrinter(), null).toString();
+            Assert.assertNotNull(pp);
+            Assert.assertTrue(pp.length() > 0);
+            final String pppp = grammar.parse(pp, Nonterminal.SENTENCE).accept(grammar.getPrettyPrinter(), null).toString();
             Assert.assertEquals(pp, pppp);
         }
         {
-            final String pp = PrettyPrinter.print(grammar.parse("HeroIcxO TrovAs Fin SkribIlO Kaj HeroIcxO DonAs TestO$Al HeroInO Fin SkribIlO .", Nonterminal.SENTENCE)).toString();
-            final String pppp = PrettyPrinter.print(grammar.parse(pp, Nonterminal.SENTENCE)).toString();
+            final String pp = grammar.parse("HeroIcxO TrovAs Fin SkribIlO Kaj HeroIcxO DonAs TestO$Al HeroInO Fin SkribIlO .", Nonterminal.SENTENCE).accept(grammar.getPrettyPrinter(), null).toString();
+            Assert.assertNotNull(pp);
+            Assert.assertTrue(pp.length() > 0);
+            final String pppp = grammar.parse(pp, Nonterminal.SENTENCE).accept(grammar.getPrettyPrinter(), null).toString();
             Assert.assertEquals(pp, pppp);
         }
         {
-            final String pp = PrettyPrinter.print(grammar.parse("{\n\tHeroIcxO TrovAs Fin SkribIlO\n} Kaj {\n\tHeroIcxO DonAs Al HeroInO Fin SkribIlO\n} .", Nonterminal.SENTENCE)).toString();
-            final String pppp = PrettyPrinter.print(grammar.parse(pp, Nonterminal.SENTENCE)).toString();
+            final String pp = grammar.parse("{\n\tHeroIcxO TrovAs Fin SkribIlO\n} Kaj {\n\tHeroIcxO DonAs Al HeroInO Fin SkribIlO\n} .", Nonterminal.SENTENCE).accept(grammar.getPrettyPrinter(), null).toString();
+            Assert.assertNotNull(pp);
+            Assert.assertTrue(pp.length() > 0);
+            final String pppp = grammar.parse(pp, Nonterminal.SENTENCE).accept(grammar.getPrettyPrinter(), null).toString();
             Assert.assertEquals(pp, pppp);
         }
         {
-            final String pp = PrettyPrinter.print(grammar.parse("{\n\tTestO$Su ( HeroIcxO )\n\tDom ( TrovAntAs )\n\tFin ( SkribIlO )\n} Kaj {\n\tSu ( HeroIcxO )\n\tDom ( DonAs )\n\tAl ( HeroInO )\n\tFin ( SkribIlO )\n} .", Nonterminal.SENTENCE)).toString();
-            final String pppp = PrettyPrinter.print(grammar.parse(pp, Nonterminal.SENTENCE)).toString();
+            final String pp = grammar.parse("{\n\tTestO$Su ( HeroIcxO )\n\tDom ( TrovAntAs )\n\tFin ( SkribIlO )\n} Kaj {\n\tSu ( HeroIcxO )\n\tDom ( DonAs )\n\tAl ( HeroInO )\n\tFin ( SkribIlO )\n} .", Nonterminal.SENTENCE).accept(grammar.getPrettyPrinter(), null).toString();
+            Assert.assertNotNull(pp);
+            Assert.assertTrue(pp.length() > 0);
+            final String pppp = grammar.parse(pp, Nonterminal.SENTENCE).accept(grammar.getPrettyPrinter(), null).toString();
             Assert.assertEquals(pp, pppp);
         }
         {
-            final String pp = PrettyPrinter.print(grammar.parse("HeroIcxO DonAs Al HeroInO Fin FlorO Se HeroIcxO TrovAs Fin FlorO .", Nonterminal.SENTENCE)).toString();
-            final String pppp = PrettyPrinter.print(grammar.parse(pp, Nonterminal.SENTENCE)).toString();
+            final String pp = grammar.parse("HeroIcxO DonAs Al HeroInO Fin FlorO Se HeroIcxO TrovAs Fin FlorO .", Nonterminal.SENTENCE).accept(grammar.getPrettyPrinter(), null).toString();
+            Assert.assertNotNull(pp);
+            Assert.assertTrue(pp.length() > 0);
+            final String pppp = grammar.parse(pp, Nonterminal.SENTENCE).accept(grammar.getPrettyPrinter(), null).toString();
             Assert.assertEquals(pp, pppp);
         }
         {
-            final String pp = PrettyPrinter.print(grammar.parse("Se HeroIcxO TrovAs Fin FlorO { HeroIcxO DonAs Al HeroInO Fin FlorO } .", Nonterminal.SENTENCE)).toString();
-            final String pppp = PrettyPrinter.print(grammar.parse(pp, Nonterminal.SENTENCE)).toString();
+            final String pp = grammar.parse("Se HeroIcxO TrovAs Fin FlorO { HeroIcxO DonAs Al HeroInO Fin FlorO } .", Nonterminal.SENTENCE).accept(grammar.getPrettyPrinter(), null).toString();
+            Assert.assertNotNull(pp);
+            Assert.assertTrue(pp.length() > 0);
+            final String pppp = grammar.parse(pp, Nonterminal.SENTENCE).accept(grammar.getPrettyPrinter(), null).toString();
             Assert.assertEquals(pp, pppp);
         }
         {
-            final String pp = PrettyPrinter.print(grammar.parse("Se { HeroIcxO TrovAs Fin FlorO } { HeroIcxO DonAs Al HeroInO Fin FlorO } .", Nonterminal.SENTENCE)).toString();
-            final String pppp = PrettyPrinter.print(grammar.parse(pp, Nonterminal.SENTENCE)).toString();
+            final String pp = grammar.parse("Se { HeroIcxO TrovAs Fin FlorO } { HeroIcxO DonAs Al HeroInO Fin FlorO } .", Nonterminal.SENTENCE).accept(grammar.getPrettyPrinter(), null).toString();
+            Assert.assertNotNull(pp);
+            Assert.assertTrue(pp.length() > 0);
+            final String pppp = grammar.parse(pp, Nonterminal.SENTENCE).accept(grammar.getPrettyPrinter(), null).toString();
             Assert.assertEquals(pp, pppp);
         }
     }
