c32fa7646b16fde8782d3afff379ee9137cfd8b4
Stefan K. Baur <info@stefan-baur.org>
2016-08-13 13:49:32 +0200

    Pretty-printing without any sugar (PrettyPrinter vs. SugarPrinter)

--- src/main/java/org/jesadido/poc/core/syntax/tree/visitors/PrettyPrinter.java
+++ src/main/java/org/jesadido/poc/core/syntax/tree/visitors/PrettyPrinter.java
@@ -8,7 +8,6 @@
 package org.jesadido.poc.core.syntax.tree.visitors;
 
 import java.util.logging.Logger;
-import org.jesadido.poc.core.concepts.Concept;
 import org.jesadido.poc.core.concepts.ConceptUtils;
 import org.jesadido.poc.core.scripting.Src;
 import org.jesadido.poc.core.syntax.Grammar;
@@ -29,126 +28,117 @@ import org.jesadido.poc.core.syntax.tree.sentence.SubstantiveSelection;
 import org.jesadido.poc.core.syntax.tree.sentence.VerbSelection;
 import org.jesadido.poc.core.syntax.tree.sentence.VerbalSelection;
 
-public class PrettyPrinter implements Visitor<Src, Boolean> {
+public class PrettyPrinter implements Visitor<Src, Void> {
     
-    public static final Src print(final Node node, final boolean sugared) {
-        return node.accept(new PrettyPrinter(), sugared);
+    public static final Src print(final Node node) {
+        return node.accept(new PrettyPrinter(), null);
     }
     
     @Override
-    public Src visit(final Sentence node, final Boolean sugared) {
+    public Src visit(final Sentence node, final Void unused) {
         Src result = new Src();
-        node.getMeats().stream().forEach(meat -> result.add(meat.accept(this, sugared)));
+        node.getMeats().stream().forEach(meat -> result.add(meat.accept(this, unused)));
         result.line("%s", node.getTerminator().getConcept());
         return result;
     }
 
     @Override
-    public Src visit(final SentenceMeat node, final Boolean sugared) {
+    public Src visit(final SentenceMeat node, final Void unused) {
         Src result = new Src();
         if (node.hasConjunction()) {
-            result.add(node.getConjunction().accept(this, sugared));
+            result.add(node.getConjunction().accept(this, unused));
         }
         result.begin("%s", node.getOpener().getConcept());
-        node.getParts().stream().forEach(part -> result.add(part.accept(this, sugared)));
+        node.getParts().stream().forEach(part -> result.add(part.accept(this, unused)));
         result.end("%s", node.getCloser().getConcept());
         return result;
     }
 
     @Override
-    public Src visit(final SentenceMeatConjunction node, final Boolean sugared) {
+    public Src visit(final SentenceMeatConjunction node, final Void unused) {
         Src result = new Src();
         result.line("%s", node.getConjunction().getConcept());
         return result;
     }
 
     @Override
-    public Src visit(final PartSu node, final Boolean sugared) {
+    public Src visit(final PartSu node, final Void unused) {
         Src result = new Src();
-        if (sugared) {
-            final Concept prepositionConcept = node.getPreposition().isDefault() ? null : node.getPreposition().getConcept();
-            result.begin("%s", ConceptUtils.join(prepositionConcept, node.getOpener().getConcept()));
-            if (node.hasNominalSelection()) {
-                result.add(node.getNominalSelection().accept(this, sugared));
-            }
-            result.end("%s", node.getCloser().getConcept());
-        } else {
-            result.begin("%s", ConceptUtils.join(node.getPreposition().getConcept(), node.getOpener().getConcept()));
-            if (node.hasNominalSelection()) {
-                result.add(node.getNominalSelection().accept(this, sugared));
-            }
-            result.end("%s", node.getCloser().getConcept());
+        result.begin("%s", ConceptUtils.join(node.getPreposition().getConcept(), node.getOpener().getConcept()));
+        if (node.hasNominalSelection()) {
+            result.add(node.getNominalSelection().accept(this, unused));
         }
+        result.end("%s", node.getCloser().getConcept());
         return result;
     }
 
     @Override
-    public Src visit(final PartDom node, final Boolean sugared) {
+    public Src visit(final PartDom node, final Void unused) {
         Src result = new Src();
         result.begin("%s", ConceptUtils.join(node.getPreposition().getConcept(), node.getOpener().getConcept()));
         if (node.hasVerbalSelection()) {
-            result.add(node.getVerbalSelection().accept(this, sugared));
+            result.add(node.getVerbalSelection().accept(this, unused));
         }
         result.end("%s", node.getCloser().getConcept());
         return result;
     }
 
     @Override
-    public Src visit(final PartAl node, final Boolean sugared) {
+    public Src visit(final PartAl node, final Void unused) {
         Src result = new Src();
         result.begin("%s", ConceptUtils.join(node.getPreposition().getConcept(), node.getOpener().getConcept()));
         if (node.hasNominalSelection()) {
-            result.add(node.getNominalSelection().accept(this, sugared));
+            result.add(node.getNominalSelection().accept(this, unused));
         }
         result.end("%s", node.getCloser().getConcept());
         return result;
     }
 
     @Override
-    public Src visit(final PartFin node, final Boolean sugared) {
+    public Src visit(final PartFin node, final Void unused) {
         Src result = new Src();
         result.begin("%s", ConceptUtils.join(node.getPreposition().getConcept(), node.getOpener().getConcept()));
         if (node.hasNominalSelection()) {
-            result.add(node.getNominalSelection().accept(this, sugared));
+            result.add(node.getNominalSelection().accept(this, unused));
         }
         result.end("%s", node.getCloser().getConcept());
         return result;
     }
 
     @Override
-    public Src visit(final NominalSelection node, final Boolean sugared) {
+    public Src visit(final NominalSelection node, final Void unused) {
         Src result = new Src();
         if (node.hasSubstantiveSelection()) {
-            result.add(node.getSubstantiveSelection().accept(this, sugared));
+            result.add(node.getSubstantiveSelection().accept(this, unused));
         }
         return result;
     }
 
     @Override
-    public Src visit(final SubstantiveSelection node, final Boolean sugared) {
+    public Src visit(final SubstantiveSelection node, final Void unused) {
         Src result = new Src();
         result.line("%s", node.getSubstantive().getConcept());
         return result;
     }
 
     @Override
-    public Src visit(final VerbalSelection node, final Boolean sugared) {
+    public Src visit(final VerbalSelection node, final Void unused) {
         Src result = new Src();
         if (node.hasVerbSelection()) {
-            result.add(node.getVerbSelection().accept(this, sugared));
+            result.add(node.getVerbSelection().accept(this, unused));
         }
         return result;
     }
 
     @Override
-    public Src visit(final VerbSelection node, final Boolean sugared) {
+    public Src visit(final VerbSelection node, final Void unused) {
         Src result = new Src();
         result.line("%s", node.getVerb().getConcept());
         return result;
     }
 
     @Override
-    public Src visit(final TroubleNode node, final Boolean sugared) {
+    public Src visit(final TroubleNode node, final Void unused) {
         return new Src();
     }
     
@@ -161,22 +151,19 @@ public class PrettyPrinter implements Visitor<Src, Boolean> {
             "{\n\tHeroIcxO TrovAs Fin SkribIlO\n} Kaj {\n\tHeroIcxO DonAs Al HeroInO Fin SkribIlO\n} .",
             "{\n\tTestO$Su ( HeroIcxO )\n\tDom ( TrovAntAs )\n\tFin ( SkribIlO )\n} Kaj {\n\tSu ( HeroIcxO )\n\tDom ( DonAs )\n\tAl ( HeroInO )\n\tFin ( SkribIlO )\n} ."
         }) {
-            for (final Boolean sugared : new Boolean[] { false, true }) {
-                final Node sentenceOriginal = grammar.parse(sentencePhrase, Nonterminal.SENTENCE);
-                final Src prettyPrintOriginal = PrettyPrinter.print(sentenceOriginal, sugared);
-                final Node sentencePrettyPrintOriginal = grammar.parse(prettyPrintOriginal.toString(), Nonterminal.SENTENCE);
-                final Src prettyPrintPrettyPrintOriginal = PrettyPrinter.print(sentencePrettyPrintOriginal, sugared);
-                final Src compressed0 = new Src(0).add(prettyPrintPrettyPrintOriginal);
-                final Src compressed1 = new Src(1).add(prettyPrintPrettyPrintOriginal);
-                Logger.getAnonymousLogger().info(
-                        "Sugared=".concat(sugared.toString()).concat(":\n\n")
-                        .concat("Original-Phrase:\n\n").concat(sentencePhrase).concat("\n\n")
-                        .concat("Pretty-Print:\n\n").concat(prettyPrintOriginal.toString()).concat("\n\n")
-                        .concat("Pretty-Print of Pretty-Print:\n\n").concat(prettyPrintPrettyPrintOriginal.toString()).concat("\n\n")
-                        .concat("Compression-Level=0:\n\n").concat(compressed0.toString()).concat("\n\n")
-                        .concat("Compression-Level=1:\n\n").concat(compressed1.toString())
-                );
-            }
+            final Node sentenceOriginal = grammar.parse(sentencePhrase, Nonterminal.SENTENCE);
+            final Src prettyPrintOriginal = PrettyPrinter.print(sentenceOriginal);
+            final Node sentencePrettyPrintOriginal = grammar.parse(prettyPrintOriginal.toString(), Nonterminal.SENTENCE);
+            final Src prettyPrintPrettyPrintOriginal = PrettyPrinter.print(sentencePrettyPrintOriginal);
+            final Src compressed0 = new Src(0).add(prettyPrintPrettyPrintOriginal);
+            final Src compressed1 = new Src(1).add(prettyPrintPrettyPrintOriginal);
+            Logger.getAnonymousLogger().info(
+                    "Original-Phrase:\n\n".concat(sentencePhrase).concat("\n\n")
+                    .concat("Pretty-Print:\n\n").concat(prettyPrintOriginal.toString()).concat("\n\n")
+                    .concat("Pretty-Print of Pretty-Print:\n\n").concat(prettyPrintPrettyPrintOriginal.toString()).concat("\n\n")
+                    .concat("Compression-Level=0:\n\n").concat(compressed0.toString()).concat("\n\n")
+                    .concat("Compression-Level=1:\n\n").concat(compressed1.toString())
+            );
         }
     }
 }
