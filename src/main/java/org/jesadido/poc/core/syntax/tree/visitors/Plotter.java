752830c70b8f99be915625217f464e53f9a8a584
Stefan K. Baur <info@stefan-baur.org>
2016-08-17 11:58:05 +0200

    Optimizing of 'Plotter'

--- src/main/java/org/jesadido/poc/core/syntax/tree/visitors/Plotter.java
+++ src/main/java/org/jesadido/poc/core/syntax/tree/visitors/Plotter.java
@@ -13,6 +13,7 @@ import org.jesadido.poc.core.syntax.Grammar;
 import org.jesadido.poc.core.syntax.GrammarFactory;
 import org.jesadido.poc.core.syntax.Nonterminal;
 import org.jesadido.poc.core.syntax.tree.Node;
+import org.jesadido.poc.core.syntax.tree.NominalPartNode;
 import org.jesadido.poc.core.syntax.tree.Terminal;
 import org.jesadido.poc.core.syntax.tree.Visitor;
 import org.jesadido.poc.core.syntax.tree.TroubleNode;
@@ -28,142 +29,150 @@ import org.jesadido.poc.core.syntax.tree.sentence.SentenceMeatConjunction;
 import org.jesadido.poc.core.syntax.tree.sentence.VerbSelection;
 import org.jesadido.poc.core.syntax.tree.sentence.VerbalSelection;
 
-public class Plotter implements Visitor<Src, Void> {
+public class Plotter implements Visitor<Void, Src> {
     
     public static final Src plot(final Node node) {
-        return node.accept(new Plotter(), null);
+        Src result = new Src();
+        node.accept(new Plotter(), result);
+        return result;
     }
     
-    private Src openSrc(final Node node) {
-        return new Src().line("• %s", node.getClass().getSimpleName()).inc();
+    private static String intro(final Node node) {
+        return String.format("• %s", node.getClass().getSimpleName());
     }
     
-    private Src closeSrc(final Src src) {
-        return src.dec();
-    }
-    
-    private void out(final Src src, final Terminal terminal) {
+    private static String terminal(final Terminal terminal) {
         if (terminal.hasToken()) {
-            src.line("%s", terminal.getToken());
+            return String.format("%s", terminal.getToken());
         } else if (terminal.hasConcept()) {
-            src.line("\"%s\"", terminal.getConcept().getFullPhrase());
+            return String.format("\"%s\"", terminal.getConcept().getFullPhrase());
         } else {
-            src.line("•••");
+            return "•••";
         }
     }
     
-    @Override
-    public Src visit(final Sentence node, final Void unused) {
-        Src result = this.openSrc(node);
-        node.getMeats().stream().forEach(meat -> result.add(meat.accept(this, unused)));
-        this.out(result, node.getTerminator());
-        return this.closeSrc(result);
+    private Void visitNominalPartNode(final NominalPartNode node, final Src result) {
+        result.line(intro(node));
+        result.inc();
+        result.line("%s", terminal(node.getPreposition()));
+        result.line("%s", terminal(node.getOpener()));
+        if (node.hasNominalSelection()) {
+            node.getNominalSelection().accept(this, result);
+        }
+        result.line("%s", terminal(node.getCloser()));
+        result.dec();
+        return null;
     }
     
     @Override
-    public Src visit(final SentenceMeat node, final Void unused) {
-        Src result = this.openSrc(node);
+    public Void visit(final Sentence node, final Src result) {
+        result.line(intro(node));
+        result.inc();
+        node.getMeats().stream().forEach(meat -> meat.accept(this, result));
+        result.line("%s", terminal(node.getTerminator()));
+        result.dec();
+        return null;
+    }
+    
+    @Override
+    public Void visit(final SentenceMeat node, final Src result) {
+        result.line(intro(node));
+        result.inc();
         if (node.hasConjunction()) {
-            result.add(node.getConjunction().accept(this, unused));
+            node.getConjunction().accept(this, result);
         }
-        this.out(result, node.getOpener());
-        node.getParts().stream().forEach(part -> result.add(part.accept(this, unused)));
-        this.out(result, node.getCloser());
-        return this.closeSrc(result);
+        result.line("%s", terminal(node.getOpener()));
+        node.getParts().stream().forEach(part -> part.accept(this, result));
+        result.line("%s", terminal(node.getCloser()));
+        result.dec();
+        return null;
     }
     
     @Override
-    public Src visit(final SentenceMeatConjunction node, final Void unused) {
-        Src result = this.openSrc(node);
-        this.out(result, node.getConjunction());
-        return this.closeSrc(result);
+    public Void visit(final SentenceMeatConjunction node, final Src result) {
+        result.line(intro(node));
+        result.inc();
+        result.line("%s", terminal(node.getConjunction()));
+        result.dec();
+        return null;
     }
     
     @Override
-    public Src visit(final PartSu node, final Void unused) {
-        Src result = this.openSrc(node);
-        this.out(result, node.getPreposition());
-        this.out(result, node.getOpener());
-        if (node.hasNominalSelection()) {
-            result.add(node.getNominalSelection().accept(this, unused));
-        }
-        this.out(result, node.getCloser());
-        return this.closeSrc(result);
+    public Void visit(final PartSu node, final Src result) {
+        return this.visitNominalPartNode(node, result);
     }
     
     @Override
-    public Src visit(final PartDom node, final Void unused) {
-        Src result = this.openSrc(node);
-        this.out(result, node.getPreposition());
-        this.out(result, node.getOpener());
+    public Void visit(final PartDom node, final Src result) {
+        result.line(intro(node));
+        result.inc();
+        result.line("%s", terminal(node.getPreposition()));
+        result.line("%s", terminal(node.getOpener()));
         if (node.hasVerbalSelection()) {
-            result.add(node.getVerbalSelection().accept(this, unused));
+            node.getVerbalSelection().accept(this, result);
         }
-        this.out(result, node.getCloser());
-        return this.closeSrc(result);
+        result.line("%s", terminal(node.getCloser()));
+        result.dec();
+        return null;
     }
     
     @Override
-    public Src visit(final PartAl node, final Void unused) {
-        Src result = this.openSrc(node);
-        this.out(result, node.getPreposition());
-        this.out(result, node.getOpener());
-        if (node.hasNominalSelection()) {
-            result.add(node.getNominalSelection().accept(this, unused));
-        }
-        this.out(result, node.getCloser());
-        return this.closeSrc(result);
+    public Void visit(final PartAl node, final Src result) {
+        return this.visitNominalPartNode(node, result);
     }
     
     @Override
-    public Src visit(final PartFin node, final Void unused) {
-        Src result = this.openSrc(node);
-        this.out(result, node.getPreposition());
-        this.out(result, node.getOpener());
-        if (node.hasNominalSelection()) {
-            result.add(node.getNominalSelection().accept(this, unused));
-        }
-        this.out(result, node.getCloser());
-        return this.closeSrc(result);
+    public Void visit(final PartFin node, final Src result) {
+        return this.visitNominalPartNode(node, result);
     }
     
     @Override
-    public Src visit(final NominalSelection node, final Void unused) {
-        Src result = this.openSrc(node);
+    public Void visit(final NominalSelection node, final Src result) {
+        result.line(intro(node));
+        result.inc();
         if (node.hasSubstantiveSelection()) {
-            result.add(node.getSubstantiveSelection().accept(this, unused));
+            node.getSubstantiveSelection().accept(this, result);
         }
-        return this.closeSrc(result);
+        result.dec();
+        return null;
     }
     
     @Override
-    public Src visit(final SubstantiveSelection node, final Void unused) {
-        Src result = this.openSrc(node);
-        this.out(result, node.getSubstantive());
-        return this.closeSrc(result);
+    public Void visit(final SubstantiveSelection node, final Src result) {
+        result.line(intro(node));
+        result.inc();
+        result.line("%s", terminal(node.getSubstantive()));
+        result.dec();
+        return null;
     }
     
     @Override
-    public Src visit(final VerbalSelection node, final Void unused) {
-        Src result = this.openSrc(node);
+    public Void visit(final VerbalSelection node, final Src result) {
+        result.line(intro(node));
+        result.inc();
         if (node.hasVerbSelection()) {
-            result.add(node.getVerbSelection().accept(this, unused));
+            node.getVerbSelection().accept(this, result);
         }
-        return this.closeSrc(result);
+        result.dec();
+        return null;
     }
     
     @Override
-    public Src visit(final VerbSelection node, final Void unused) {
-        Src result = this.openSrc(node);
-        this.out(result, node.getVerb());
-        return this.closeSrc(result);
+    public Void visit(final VerbSelection node, final Src result) {
+        result.line(intro(node));
+        result.inc();
+        result.line("%s", terminal(node.getVerb()));
+        result.dec();
+        return null;
     }
     
     @Override
-    public Src visit(final TroubleNode node, final Void unused) {
-        Src result = this.openSrc(node);
+    public Void visit(final TroubleNode node, final Src result) {
+        result.line(intro(node));
+        result.inc();
         result.line("••• %s", node.getMessage());
-        return this.closeSrc(result);
+        result.dec();
+        return null;
     }
     
     public static void main(final String[] arguments) {
