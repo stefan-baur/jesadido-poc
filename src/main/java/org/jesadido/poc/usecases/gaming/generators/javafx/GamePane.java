376c95e9436e80e75d952b2f0db4e86da1f7f845
Stefan K. Baur <info@stefan-baur.org>
2016-10-05 10:59:45 +0200

    The 'GameScene' class is added and used.

--- src/main/java/org/jesadido/poc/usecases/gaming/generators/javafx/GamePane.java
+++ src/main/java/org/jesadido/poc/usecases/gaming/generators/javafx/GamePane.java
@@ -9,70 +9,13 @@ package org.jesadido.poc.usecases.gaming.generators.javafx;
 
 import javafx.beans.value.ObservableValue;
 import javafx.geometry.Bounds;
-import javafx.geometry.VPos;
-import javafx.scene.Group;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Line;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Text;
-import org.jesadido.poc.JesadidoPoc;
-import org.jesadido.poc.core.Language;
-import org.jesadido.poc.core.scripting.Src;
-import org.jesadido.poc.core.semantics.translating.TranslatorFactory;
 import org.jesadido.poc.usecases.gaming.models.GameModel;
 
 public class GamePane extends Pane {
     
-    private final GameState gameState;
-    
-    private final Group gameScene = new Group();
-    
     public GamePane(final GameModel gameModel) {
-        this.gameState = new GameState(gameModel);
-        this.init();
-        this.layoutBoundsProperty().addListener((ObservableValue<? extends Bounds> observable, Bounds oldValue, Bounds newValue) -> this.invalidate());
-    }
-    
-    private void init() {
-        this.getChildren().add(this.gameScene);
-    }
-    
-    private void invalidate() {
-        final double clientWidth = Math.max(200, this.getWidth());
-        final double clientHeight = Math.max(160, this.getHeight());
-        this.gameScene.getChildren().clear();
-        
-        this.gameScene.getChildren().add(new Rectangle(clientWidth, clientHeight, Color.DARKGREEN));
-        
-        final Line vLine = new Line(clientWidth / 2.0, 0, clientWidth / 2.0, clientHeight);
-        vLine.setStroke(Color.GREEN);
-        this.gameScene.getChildren().add(vLine);
-        
-        final Line hLine = new Line(0, clientHeight / 2.0, clientWidth, clientHeight / 2.0);
-        hLine.setStroke(Color.GREEN);
-        this.gameScene.getChildren().add(hLine);
-        
-        final Text supportedLanguages = new Text(String.format("%s %s %s", this.gameState.getMainLanguage(), this.gameState.getSemiLanguages(), this.gameState.getSelectableLanguages().toString().replace("[", "{").replace("]", "}")));
-        supportedLanguages.setFill(Color.WHITE);
-        supportedLanguages.setX(3);
-        supportedLanguages.setY(2);
-        supportedLanguages.setTextOrigin(VPos.TOP);
-        this.gameScene.getChildren().add(supportedLanguages);
-        
-        final Text title = new Text(this.translate(this.gameState.getMainLanguage(), this.gameState.getGameModel().getTitle().getSource()));
-        title.setFill(Color.WHITE);
-        title.setX((clientWidth - title.prefWidth(-1)) / 2.0);
-        title.setY(clientHeight / 2.0);
-        title.setTextOrigin(VPos.CENTER);
-        this.gameScene.getChildren().add(title);
-    }
-    
-    private String translate(final Language language, final String source) {
-        return TranslatorFactory.createTranslator(language, this.gameState.getGameModel().getGameConceptBook()).translate(source).getTranslation();
-    }
-    
-    public static void main(final String[] arguments) {
-        JesadidoPoc.run(arguments);
+        super(new GameScene(gameModel));
+        this.layoutBoundsProperty().addListener((ObservableValue<? extends Bounds> observable, Bounds oldValue, Bounds newValue) -> this.getChildren().get(0).resize(newValue.getWidth(), newValue.getHeight()));
     }
 }
---
+++ src/main/java/org/jesadido/poc/usecases/gaming/generators/javafx/GameScene.java
@@ -0,0 +1,100 @@
+/*
+ * jesadido-poc
+ * Copyright (C) 2016 Stefan K. Baur
+ *
+ * Licensed under the GNU Lesser General Public License, Version 3.0 (LGPL-3.0)
+ * https://www.gnu.org/licenses/lgpl-3.0.txt
+ */
+package org.jesadido.poc.usecases.gaming.generators.javafx;
+
+import javafx.geometry.VPos;
+import javafx.scene.Group;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import org.jesadido.poc.JesadidoPoc;
+import org.jesadido.poc.core.Language;
+import org.jesadido.poc.core.semantics.translating.TranslatorFactory;
+import org.jesadido.poc.usecases.gaming.models.GameModel;
+
+public class GameScene extends Group {
+    
+    private final GameState gameState;
+    
+    private static final double MIN_WIDTH = 200;
+    private static final double MIN_HEIGHT = 160;
+    
+    private double width = MIN_WIDTH;
+    private double height = MIN_HEIGHT;
+    
+    public GameScene(final GameModel gameModel) {
+        this.gameState = new GameState(gameModel);
+        this.init();
+    }
+    
+    public GameState getGameState() {
+        return this.gameState;
+    }
+    
+    public GameModel getGameModel() {
+        return this.gameState.getGameModel();
+    }
+    
+    public double getWidth() {
+        return this.width;
+    }
+    
+    public double getHeight() {
+        return this.height;
+    }
+    
+    public void setSize(final double width, final double height) {
+        this.width = Math.max(MIN_WIDTH, width);
+        this.height = Math.max(MIN_HEIGHT, height);
+    }
+    
+    private void init() {
+        final double w = this.getWidth();
+        final double h = this.getHeight();
+        this.getChildren().add(new Rectangle(w, h, Color.DARKGREEN));
+        
+        final Line vLine = new Line(w / 2.0, 0, w / 2.0, h);
+        vLine.setStroke(Color.GREEN);
+        this.getChildren().add(vLine);
+        
+        final Line hLine = new Line(0, h / 2.0, w, h / 2.0);
+        hLine.setStroke(Color.GREEN);
+        this.getChildren().add(hLine);
+        
+        final Text languageSettings = new Text(String.format("%s %s %s", this.gameState.getMainLanguage(), this.gameState.getSemiLanguages(), this.gameState.getSelectableLanguages().toString().replace("[", "{").replace("]", "}")));
+        languageSettings.setFill(Color.WHITE);
+        languageSettings.setX(3);
+        languageSettings.setY(2);
+        languageSettings.setTextOrigin(VPos.TOP);
+        this.getChildren().add(languageSettings);
+        
+        final Text title = new Text(this.translate(this.gameState.getMainLanguage(), this.gameState.getGameModel().getTitle().getSource()));
+        title.setFill(Color.WHITE);
+        title.setX((w - title.prefWidth(-1)) / 2.0);
+        title.setY(h / 2.0);
+        title.setTextOrigin(VPos.CENTER);
+        this.getChildren().add(title);
+    }
+    
+    @Override
+    public void resize(final double width, final double height) {
+        super.resize(width, height);
+        this.setSize(width, height);
+        this.getChildren().clear();
+        this.init();
+    }
+    
+    private String translate(final Language language, final String source) {
+        return TranslatorFactory.createTranslator(language, this.gameState.getGameModel().getGameConceptBook()).translate(source).getTranslation();
+    }
+    
+    public static void main(final String[] arguments) {
+        JesadidoPoc.run(arguments);
+    }
+}
