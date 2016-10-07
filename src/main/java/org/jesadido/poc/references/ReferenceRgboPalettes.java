ff2f4b778a7cfbf82fa1834792071ff46963b56d
Stefan K. Baur <info@stefan-baur.org>
2016-10-07 11:55:37 +0200

    Refactoring and renaming and better colors

--- src/main/java/org/jesadido/poc/references/ReferenceRgboPalettes.java
+++ src/main/java/org/jesadido/poc/references/ReferenceRgboPalettes.java
@@ -11,7 +11,7 @@ import org.jesadido.poc.JesadidoPoc;
 import org.jesadido.poc.usecases.gaming.graphics.RgboKeys;
 import org.jesadido.poc.usecases.gaming.graphics.RgboPalette;
 
-public final class ReferenceRgbos {
+public final class ReferenceRgboPalettes {
     
     public static final RgboPalette DEFAULT_COLORS = new RgboPalette("default-colors")
             .add(RgboKeys.BACKGROUND_FILL, 44, 44, 44)
@@ -19,18 +19,23 @@ public final class ReferenceRgbos {
             .add(RgboKeys.MAIN_LANGUAGE_FILL, 240, 230, 140)
             .add(RgboKeys.MAIN_TEXT_FILL, 240, 230, 140)
             .add(RgboKeys.SEMI_LANGUAGE_CONTROL_FILL, 55, 55, 55, 0.5)
-            .add(RgboKeys.SEMI_LANGUAGE_FILL, 210, 210, 210)
-            .add(RgboKeys.SEMI_TEXT_FILL, 210, 210, 210)
+            .add(RgboKeys.SEMI_LANGUAGE_FILL, 200, 200, 200)
+            .add(RgboKeys.SEMI_TEXT_FILL, 220, 220, 220)
             .add(RgboKeys.REST_LANGUAGE_CONTROL_FILL, 55, 55, 55, 0.3)
             .add(RgboKeys.REST_LANGUAGE_FILL, 66, 66, 66)
             .add(RgboKeys.REST_TEXT_FILL, 66, 66, 66)
             ;
     
     public static final RgboPalette MAGIC_COLORS = new RgboPalette("magic-colors").addAll(DEFAULT_COLORS)
-            .add(RgboKeys.BACKGROUND_FILL, 00, 22, 55)
+            .add(RgboKeys.BACKGROUND_FILL, 0, 22, 55)
+            .add(RgboKeys.MAIN_LANGUAGE_CONTROL_FILL, 88, 88, 88, 0.25)
+            .add(RgboKeys.SEMI_LANGUAGE_CONTROL_FILL, 88, 88, 88, 0.2)
+            .add(RgboKeys.SEMI_TEXT_FILL, 240, 240, 240)
+            .add(RgboKeys.REST_LANGUAGE_CONTROL_FILL, 88, 88, 88, 0.15)
+            .add(RgboKeys.REST_LANGUAGE_FILL, 70, 77, 88, 0.8)
             ;
     
-    private ReferenceRgbos() {
+    private ReferenceRgboPalettes() {
         // A private class constructor
     }
     
