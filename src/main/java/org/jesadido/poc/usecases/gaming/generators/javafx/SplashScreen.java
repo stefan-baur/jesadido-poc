ac8c710f9ff600ee126a1beb968ab8f649447b60
Stefan K. Baur <info@stefan-baur.org>
2016-10-07 08:20:50 +0200

    Title-output with semi-language-translations

--- src/main/java/org/jesadido/poc/usecases/gaming/generators/javafx/SplashScreen.java
+++ src/main/java/org/jesadido/poc/usecases/gaming/generators/javafx/SplashScreen.java
@@ -7,16 +7,19 @@
  */
 package org.jesadido.poc.usecases.gaming.generators.javafx;
 
+import java.util.EnumMap;
+import java.util.Map;
 import javafx.geometry.VPos;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import org.jesadido.poc.core.Language;
 import org.jesadido.poc.core.semantics.translating.TranslatorFactory;
 
 public class SplashScreen extends SizedGameObject {
     
-    private final Text title = new Text();
+    private final Map<Language, Text> title = new EnumMap<>(Language.class);
     
     public SplashScreen(final GameScene gameScene) {
         super(gameScene, 200, 160);
@@ -24,10 +27,13 @@ public class SplashScreen extends SizedGameObject {
     }
     
     private void init() {
-        this.title.setFill(Color.KHAKI);
-        this.title.setFont(new Font(20));
-        this.title.setTextOrigin(VPos.CENTER);
-        this.getChildren().add(this.title);
+        for (final Language language : Language.values()) {
+            final Text titleText = new Text(this.translate(language, this.getGameScene().getGameState().getGameModel().getTitle().getSource()));
+            titleText.setTextOrigin(VPos.CENTER);
+            this.title.put(language, titleText);
+        }
+        
+        
         this.invalidate();
     }
     
@@ -35,10 +41,29 @@ public class SplashScreen extends SizedGameObject {
     public void invalidate() {
         final double width = this.getWidth();
         final double height = this.getHeight();
-        
-        this.title.setText(this.translate(this.getGameScene().getGameState().getMainLanguage(), this.getGameScene().getGameState().getGameModel().getTitle().getSource()));
-        this.title.setX((width - this.title.prefWidth(-1)) / 2.0);
-        this.title.setY(height / 2.0);
+        this.getChildren().clear();
+        this.getChildren().add(this.createMainTitle(width, height));
+        for (int i = 0; i < this.getGameScene().getGameState().getSemiLanguages().size(); i++) {
+            this.getChildren().add(this.createSemiTitle(i, this.getGameScene().getGameState().getSemiLanguages().get(i), width, height));
+        }
+    }
+    
+    private Text createMainTitle(final double width, final double height) {
+        final Text mainTitle = this.title.get(this.getGameScene().getGameState().getMainLanguage());
+        mainTitle.setFont(Font.font("Comic Sans MS", FontWeight.NORMAL, 20));
+        mainTitle.setFill(Color.color(0.94, 0.90, 0.55));
+        mainTitle.setX((width - mainTitle.prefWidth(-1)) / 2.0);
+        mainTitle.setY(height / 3.0);
+        return mainTitle;
+    }
+    
+    private Text createSemiTitle(final int index, final Language language, final double width, final double height) {
+        final Text mainTitle = this.title.get(language);
+        mainTitle.setFont(Font.font("Comic Sans MS", FontWeight.NORMAL, 18));
+        mainTitle.setFill(Color.color(0.7, 0.7, 0.7));
+        mainTitle.setX((width - mainTitle.prefWidth(-1)) / 2.0);
+        mainTitle.setY((height / 3.0) + (index + 1) * 32);
+        return mainTitle;
     }
     
     private String translate(final Language language, final String source) {
